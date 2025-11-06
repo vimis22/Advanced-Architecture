e7ebec2a7706c01ecfd3e90afd40351886f1f7bd
SirStehn <91467050+SirStehn@users.noreply.github.com>
2025-11-06 21:34:31 +0100

    BookScheduler med MQTT
    Lavet BookScheduler om til at køre på mqtt

---
+++ src/BookScheduler_MQTT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BookScheduler
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Enter number of books to produce: ");
+            if (int.TryParse(Console.ReadLine(), out int bookCount))
+            {
+                var scheduler = new Scheduler();
+                await scheduler.ProduceBooksAsync(bookCount);
+            }
+            else
+            {
+                Console.WriteLine("Invalid number entered.");
+            }
+        }
+    }
+}
