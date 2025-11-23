8be2d874f7b1fb7428c2f966ebf630df25c5ecc3
vimis22 <vimis22@student.sdu.dk>
2025-11-23 15:39:35 +0100

    Fixed the Docker Compose Up Error

--- src/API-Gateway/src/main/java/org/advanced_architecture/gateway/config/CorsConfig.java
+++ src/API-Gateway/src/main/java/org/advanced_architecture/gateway/config/CorsConfig.java
@@ -8,17 +8,17 @@ import org.springframework.web.cors.reactive.CorsWebFilter;
 
 @Configuration
 public class CorsConfig {
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return new CorsWebFilter(source);
-    }
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.addAllowedOrigin("http://localhost:5173");
+//        config.addAllowedOrigin("http://localhost:3000");
+//        config.addAllowedMethod("*");
+//        config.addAllowedHeader("*");
+//        config.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsWebFilter(source);
+//    }
 }
